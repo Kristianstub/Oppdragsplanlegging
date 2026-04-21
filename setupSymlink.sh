b635565a8e688b7f1728e399d5f7e9fcd776da6a
toraboc <tobockman@gmail.com>
2026-04-21 17:46:47 +0200

    typo fix in symlink

--- setupSymlink.sh
+++ setupSymlink.sh
@@ -7,6 +7,6 @@ ln -sf "$(pwd)/3-PDDL" ~/catkin_ws/src/temporal-planning-main/temporal-planning/
 ln -sf "$(pwd)/6-Gazebo/turtlebot3_ttk4192.world" ~/catkin_ws/src/turtlebot3_simulations/turtlebot3_gazebo/worlds/
 ln -sf "$(pwd)/6-Gazebo/turtlebot3_ttk4192.launch" ~/catkin_ws/src/turtlebot3_simulations/turtlebot3_gazebo/launch/
 
-rm -rf ~/catkin_ws/src/assignment4_ttk419
-ln -sf "$(pwd)/mission-planner" ~/catkin_ws/src/assignment4_ttk419
+rm -rf ~/catkin_ws/src/assignment4_ttk4192
+ln -sf "$(pwd)/mission-planner" ~/catkin_ws/src/assignment4_ttk4192
 
