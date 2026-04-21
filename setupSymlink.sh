1384d3b4b6068548a59a1e5431518a2abd7222cc
Theodor Kvalsvik Lauritzen <19690242+theodorklauritzen@users.noreply.github.com>
2026-04-21 07:58:30 +0000

    Setup symlink for task 6 Gazebo

--- setupSymlink.sh
+++ setupSymlink.sh
@@ -4,4 +4,7 @@ ln -sf "$(pwd)/temporal-planning-main" ~/catkin_ws/src/
 rm -rf ~/catkin_ws/src/temporal-planning-main/temporal-planning/domains/ttk4192
 ln -sf "$(pwd)/3-PDDL" ~/catkin_ws/src/temporal-planning-main/temporal-planning/domains/ttk4192
 
+ln -sf "$(pwd)/6-Gazebo/turtlebot3_ttk4192.world" ~/catkin_ws/src/turtlebot3_simulations/turtlebot3_gazebo/worlds/
+ln -sf "$(pwd)/6-Gazebo/turtlebot3_ttk4192.launch" ~/catkin_ws/src/turtlebot3_simulations/turtlebot3_gazebo/launch/
+
 
