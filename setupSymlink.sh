950c7b8eaf69a150b1806388c480569a064f71b4
Theodor Kvalsvik Lauritzen <19690242+theodorklauritzen@users.noreply.github.com>
2026-04-21 07:27:11 +0000

    Change submodules back to ignore untracked

--- setupSymlink.sh
+++ setupSymlink.sh
@@ -1,4 +1,5 @@
 
+rm -rf ~/catkin/src/temporal-planning-main
 ln -sf "$(pwd)/temporal-planning-main" ~/catkin_ws/src/
 rm -rf ~/catkin_ws/src/temporal-planning-main/temporal-planning/domains/ttk4192
 ln -sf "$(pwd)/3-PDDL" ~/catkin_ws/src/temporal-planning-main/temporal-planning/domains/ttk4192
